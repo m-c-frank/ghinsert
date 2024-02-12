7f1fcdf0d5008665192263e78fe6dceba2fd07ec
Martin Christoph Frank <martin7.frank7@gmail.com>
2024-02-12 21:11:52 +0100

    changed to posts repository to work with m-c-frank/ghlistener

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ import os
 from watchdog.events import FileSystemEventHandler
 
 TOKEN = os.environ.get('GH_INSERT')
+TOKEN_POSTS = os.environ.get('GH_ISSUES_POSTS')
 
 
 class NewFileHandler(FileSystemEventHandler):
-    def note_issue(self, body, token=TOKEN):
+    def post_issue(self, body, timestamp, token=TOKEN_POSTS):
         repo_owner = "m-c-frank"
-        repo_name = "ghinsert"
-        title = f"note:{int(1000*time.time())}"
+        repo_name = "posts"
+        title = f":page_facing_up:{timestamp}"
+        create_github_issue(repo_owner, repo_name, title, body, token)
+
+    def note_issue(self, body, timestamp, token=TOKEN):
+        repo_owner = "m-c-frank"
+        repo_name = "notes"
+        title = f":notes:{timestamp}"
         create_github_issue(repo_owner, repo_name, title, body, token)
 
     def on_created(self, event):
         if event.is_directory:
             return
         print(f"New file created: {event.src_path}")
-        text = open(event.src_path, "r", encoding="utf-8").read()
-        self.note_issue(text)
+        if str(event.src_path).endswith(".md"):
+            timestamp = str(event.src_path).split("/")[-1].split(".")[0]
+            text = open(event.src_path, "r", encoding="utf-8").read()
+            self.post_issue(text, timestamp)
 
 
 def main():
-    directory_to_watch = "/home/mcfrank/notes"
+    directory_to_watch = "/home/mcfrank/posts"
     event_handler = NewFileHandler()
     watch_directory(directory_to_watch, event_handler)
 
